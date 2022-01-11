5d4eeceea96f4e15b974d155acb98bc617c28b4f
Piotr Korzeniowski <piotr.korzen78@gmail.com>
2022-01-11 14:23:09 +0100

    new def

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,29 +1,35 @@
 while True:
     operacja = input("Podaj dzialanie ktore chcesz wykonac : \n1.Dodawanie \n2.Odejmowanie \n3.Mnozenie \n4.Dzielenie \n5.Potegowanie \n6.Pierwiatek \n")
-    a = float(input("Podaj pierwsza liczbe: "))
+    def zapytaj_o_dwie_liczby():
+        liczba1 = float(input("Podaj pierwsza liczbe "))
+        liczba2 = float(input("Podaj druga liczbe: "))
+        return liczba1, liczba2
+    def zapyta_o_pierwsza_pierwsza():
+        a = float(input("Podaj pierwsza liczbe "))
+        return a
     if (operacja == "1"):
-        
-        b = float(input("Podaj druga liczbe: ")) 
+        a ,b = zapytaj_o_dwie_liczby()
         print(a+b)
+        
+        
+       
     elif (operacja =="2"):
         
-        b = float(input("Podaj druga liczbe: "))
+        
         print(a-b)
     elif (operacja =="3"):
        
-        b = float(input("Podaj druga liczbe: "))
+        
         print(a*b)
     elif (operacja =="4"):
         try:
             
-            b = float(input("Podaj druga liczbe: "))
             print(a/b)
         except ZeroDivisionError as Error:
             print("Bledna opreacja , nie dzielimy przez ZERO")
             
     elif (operacja =="5"):
         
-        b = float(input("Podaj druga liczbe: "))
         print(a**b)
     elif (operacja =="6"):
         
